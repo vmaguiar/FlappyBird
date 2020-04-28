9a0b1747456ba93856dc08efcf7b5b6abe9407c5
vmaguiar <vmaguiar@hotmail.com>
2020-04-28 11:02:39 -0300

    gameplay sem colisão

--- flappy.js
+++ flappy.js
@@ -66,14 +66,71 @@ function Barreiras(altura, largura, abertura, espaco, notificarPonto){
     }
 }
 
-const barreiras = new Barreiras(700, 1200, 200, 400)
-const areaDoJogo = document.querySelector('[wm-flappy]')
-barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
+function Passaro(alturaJogo){
+    let voando = false
 
-setInterval(() => {
-    barreiras.animar()
-}, 20)
+    this.elemento = novoElemento('img', 'passaro')
+    this.elemento.src = 'imgs/passaro.png'
 
+    this.getY = () => parseInt(this.elemento.style.bottom.split('px')[0])
+    this.setY = y => this.elemento.style.bottom = `${y}px`
+
+    window.onkeydown = e => voando = true
+    window.onkeyup = e => voando = false
+
+    this.animar = () => {
+        const novoY = this.getY() +(voando ? 10 : -5)
+        const alturaMaxima = alturaJogo - this.elemento.clientHeight
+
+        if(novoY <= 0){
+            this.setY(0)
+        }
+        else if(novoY >= alturaMaxima){
+            this.setY(alturaMaxima)
+        }
+        else{
+            this.setY(novoY)
+        }
+    }
+
+    this.setY(alturaJogo / 2)
+}
+
+function Progresso(){
+    this.elemento = novoElemento('span', 'progresso')
+    this.atualizarPontos = pontos => {
+        this.elemento.innerHTML = pontos
+    }
+    this.atualizarPontos(0)
+}
+
+function FlappyBird(){
+    let pontos = 0
+
+    const areaDoJogo = document.querySelector('[wm-flappy]')
+    const altura = areaDoJogo.clientHeight
+    const largura = areaDoJogo.clientWidth
+
+    const progresso = new Progresso()
+    const barreiras = new Barreiras(altura, largura, 200, 400,
+        () => progresso.atualizarPontos(++pontos))
+    const passaro = new Passaro(altura)
+
+    areaDoJogo.appendChild(passaro.elemento)
+    areaDoJogo.appendChild(progresso.elemento)
+    barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
+
+    this.start = () => {
+        // loop do jogo
+
+        const temporizador = setInterval(() => {
+            barreiras.animar()
+            passaro.animar()
+        }, 20)
+    }
+}
+
+new FlappyBird().start()
 
 // O jogo possui uma altura fixa (da div)
 // Cada barreira possui uma altura diferente
